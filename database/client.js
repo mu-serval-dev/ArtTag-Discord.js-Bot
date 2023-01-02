33d3906f429599fd73fd34a0bbcb17dbad823b88
Keith L <klowry2001@gmail.com>
2023-01-02 14:56:21 -0500

    Playing with query stuff and error handling

--- database/client.js
+++ database/client.js
@@ -1,22 +1,43 @@
 // Module containing pg database client
 
 const pg = require('pg');
+const format = require('pg-format');
 const { dbstring } = require('../config.json');
 const pgClient = new pg.Client(dbstring);
 
 pgClient.connect();
 
-const selectQuery = 'SELECT (link, $1) FROM artlinks ORDER BY ($1) DESC';
-const values = ['emoji3', 'artlinks'];
+// TODO: add error handling
+const selectQuery = format('SELECT (link, %I) FROM %I WHERE %I > 0 ORDER BY %I DESC', 'emoji3', 'artlinks', 'emoji3', 'emoji3');
+console.log(selectQuery);
 
-pgClient.query(selectQuery, values).then(result => {
-	console.log(result.rows);
-});
+async function select() {
+	pgClient.query(selectQuery).then(result => {
+		console.log(result);
+		pgClient.end();
+	}, err => {
+		const end = err.stack.indexOf('\n');
+		const errMessage = err.stack.substring(7, end);
+		console.log('Error ' + err.code + ':', errMessage);
+		pgClient.end();
+		const res = {
+			message : errMessage,
+			code : err.code,
+		};
 
-pgClient.query('SELECT emoji1 from artlinks').then(result => {
-	console.log(result.rows);
-	pgClient.end();
-});
+		return res;
+	});
+}
+
+select();
+// pgClient.query(selectQuery, values).then(result => {
+// 	console.log(result.rows);
+// });
+
+// pgClient.query('SELECT emoji1 from artlinks').then(result => {
+// 	console.log(result.rows);
+// 	pgClient.end();
+// });
 
 
 // exports.pgClient = pgClient;
