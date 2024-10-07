8b0b6aba9607a559b4c6d4c3d04acaa8c16547ac
Keith L <klowry2001@gmail.com>
2024-10-06 22:57:17 -0400

    Importing command defs

--- build/src/types.js
+++ build/src/types.js
@@ -1,4 +1,16 @@
 import { Client, Collection, SlashCommandBuilder } from "discord.js";
+/**
+ * Check if object implements the Command interface.
+ *
+ * @param object
+ * @returns True if object has Command props.
+ */
+export function isCommand(object) {
+    return 'data' in object && 'execute' in object;
+}
+/**
+ * Discord client with a collection of Commands.
+ */
 export class CommandClient extends Client {
     commands = new Collection();
 }
--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,19 @@ export interface Command {
     execute: CommandExecuteFunc
 }
 
+/**
+ * Check if object implements the Command interface.
+ * 
+ * @param object 
+ * @returns True if object has Command props.
+ */
+export function isCommand(object: any): object is Command {
+    return 'data' in object && 'execute' in object;
+}
+
+/**
+ * Discord client with a collection of Commands.
+ */
 export class CommandClient extends Client {
 	commands : Collection<string, Command> = new Collection<string,Command>()
 }
